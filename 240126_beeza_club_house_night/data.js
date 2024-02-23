4746f598cd997cc10c8b554e4a935daa5a349564
Vineeth <you@example.com>
2024-02-23 18:00:30 +0530

    new

---
+++ 240126_beeza_club_house_night/data.js
@@ -0,0 +1,363 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-night-1",
+      "name": "NIGHT-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.305405782725522,
+        "pitch": 0.005655599843393588,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4702373984804016,
+          "pitch": 0.02553819216022646,
+          "rotation": 3.141592653589793,
+          "target": "1-night-2"
+        },
+        {
+          "yaw": 2.309281830760562,
+          "pitch": 0.0022619106377383957,
+          "rotation": 3.141592653589793,
+          "target": "7-night-6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-night-2",
+      "name": "NIGHT-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0410215182090896,
+        "pitch": 0.0014924845488444305,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4173334763770598,
+          "pitch": 0.11439804566308709,
+          "rotation": 3.141592653589793,
+          "target": "2-night-3"
+        },
+        {
+          "yaw": 2.719052714717119,
+          "pitch": 0.06559555581515752,
+          "rotation": 3.141592653589793,
+          "target": "0-night-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-night-3",
+      "name": "NIGHT-3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4607113587687062,
+        "pitch": 0.013726819820377045,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2755473879120327,
+          "pitch": 0.07760629349913728,
+          "rotation": 3.141592653589793,
+          "target": "3-night-4"
+        },
+        {
+          "yaw": -2.9219813801277414,
+          "pitch": 0.09891271273297875,
+          "rotation": 3.141592653589793,
+          "target": "1-night-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-night-4",
+      "name": "NIGHT-4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4736953280622735,
+        "pitch": 0.029268423381363817,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.408520178338744,
+          "pitch": 0.025334087975068087,
+          "rotation": 0,
+          "target": "5-night-smoking-area-1"
+        },
+        {
+          "yaw": 1.1241086245176266,
+          "pitch": 0.08362224612717029,
+          "rotation": 3.141592653589793,
+          "target": "4-night-5"
+        },
+        {
+          "yaw": 2.4323082995754213,
+          "pitch": 0.13501226281542422,
+          "rotation": 3.141592653589793,
+          "target": "2-night-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-night-5",
+      "name": "NIGHT-5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.21395235113626399,
+        "pitch": 5.329070518200751e-15,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0788287113612363,
+          "pitch": 0.04088728547671394,
+          "rotation": 3.141592653589793,
+          "target": "7-night-6"
+        },
+        {
+          "yaw": 1.0886678103643312,
+          "pitch": 0.07722766004301285,
+          "rotation": 3.141592653589793,
+          "target": "3-night-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-night-smoking-area-1",
+      "name": "NIGHT-SMOKING AREA-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1839864495839407,
+        "pitch": 0.002307349422224547,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4417115990664904,
+          "pitch": -0.0016218884545669,
+          "rotation": 0,
+          "target": "3-night-4"
+        },
+        {
+          "yaw": 2.018387381182764,
+          "pitch": 0.05169583886418394,
+          "rotation": 3.141592653589793,
+          "target": "6-night-smoking-area-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-night-smoking-area-2",
+      "name": "NIGHT-SMOKING AREA-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.833628722301686,
+        "pitch": 0.0025407004057811378,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8672945539835251,
+          "pitch": -0.01131157497360391,
+          "rotation": 3.141592653589793,
+          "target": "5-night-smoking-area-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-night-6",
+      "name": "NIGHT-6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.1239451787865988,
+        "pitch": -0.0075875681701091935,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0344848912071605,
+          "pitch": 0.09521557612473153,
+          "rotation": 3.141592653589793,
+          "target": "4-night-5"
+        },
+        {
+          "yaw": 0.07969236968949289,
+          "pitch": 0.003610046843773418,
+          "rotation": 3.141592653589793,
+          "target": "0-night-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "240126_Beeza_Club_house_NIGHT",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
